7b70cd830ca42425a74853cc33562fde0fbe5298
JustSteve2k <srodriguez2k@gmail.com>
2023-02-03 01:09:36 -0600

    add cors options

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const connectDB = require("./config/db");
+var cors = require("cors");
 
 const app = express();
 
@@ -9,12 +10,26 @@ connectDB();
 const port = process.env.PORT || 5000;
 
 const productRoutes = require("./routes/productsRoute");
+//app.options("*", cors());
+//app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.options("/*", (_, res) => {
+  res.sendStatus(200);
+});
+
 app.get("/main", (req, res, next) => {
-  res.send("you've reached the main page of products api!");
+  res.status(200).send({ response: "you've reached the main page of products api!" });
 });
 
 app.use("/products", productRoutes);
